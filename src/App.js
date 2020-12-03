500a3f13be66b322c6fd38b589d0cf3df7a849cb
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-12-03 15:22:33 -0600

    Implementacion de Proteccion en la ruta login y registro

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import Login from "./components/Login";
 import Inicio from "./components/Inicio";
 import Registro from "./components/Registro";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { UserRoute } from "./routeCheckers";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { UserRoute, NoUserRoutes } from "./routeCheckers";
 
 function App() {
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/">
-                        <Login />
-                    </Route>
-                    <UserRoute path="/Inicio" component={Inicio} />
-                    <Route path="/Registro">
-                        <Registro />
-                    </Route>
+                    <UserRoute path="/inicio" component={Inicio} />
+                    <NoUserRoutes exact path="/" component={Login} />
+                    <NoUserRoutes exact path="/registro" component={Registro} />
                 </Switch>
             </div>
         </Router>
